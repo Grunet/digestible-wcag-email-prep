d48e30c3d4f6fb3e102215f23e99bafbc2754555
Grunet <sadikshahidain@gmail.com>
2020-06-21 20:58:29 -0500

    Realized the typings issues were because of the VS Code Deno extension, not Deno itself

--- src/cheerioAdapter.ts
+++ src/cheerioAdapter.ts
@@ -1,6 +1,6 @@
-//Typings don't seem to work if pointed directly at the remote url, so using a local copy instead
-// @deno-types="./typings/cheerio.d.ts"
-import cheerio from "https://dev.jspm.io/cheerio@1.0.0-rc.3"; //Doesn't get typed correctly automatically, hence the explicit typecast below
+// This typing can be switched to a more updated version once https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34036 is fixed
+// @deno-types="https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/deab75bde42b5a82aeb951f5a2edaa09922853f4/types/cheerio/index.d.ts"
+import cheerio from "https://dev.jspm.io/cheerio@1.0.0-rc.3";
 
 import { setupHtmlContainer, IContainHtml } from "./htmlAdapter.ts";
 
