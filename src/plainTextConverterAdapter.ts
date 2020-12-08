0e2bb799bbcf38d63bec6b77aea93d68d05f6ed5
Grunet <sadikshahidain@gmail.com>
2020-12-07 23:13:50 -0600

    Pinning the html-to-text typings file to the previous commit as the new v6 version has an issue with dev.jspm.io being able to fetch it

--- src/plainTextConverterAdapter.ts
+++ src/plainTextConverterAdapter.ts
@@ -1,5 +1,5 @@
-// This typing probably be pinned to a specific version in case breaking changes are made to it
-// @deno-types="https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/html-to-text/index.d.ts"
+// This is the last typing file before the 6.0.0 major version break
+// @deno-types="https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/7046815a55ef35f365e065557b405eb2f76eff67/types/html-to-text/index.d.ts"
 import htmlToText from "https://dev.jspm.io/html-to-text@5.1.1";
 
 import { IContainHtml } from "./htmlAdapter.ts";
