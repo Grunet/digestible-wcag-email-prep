8e708228e681300f3fb3aabe6ab8af78b60488d5
Grunet <sadikshahidain@gmail.com>
2024-07-17 10:07:35 -0500

    Switch to npm specifiers to fix import bug with transitive dependency domutils:1.7.0

--- dist/ts/cheerioAdapter.ts
+++ dist/ts/cheerioAdapter.ts
@@ -1,6 +1,6 @@
 // This typing can be switched to a more updated version once https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34036 is fixed
 // @deno-types="https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/deab75bde42b5a82aeb951f5a2edaa09922853f4/types/cheerio/index.d.ts"
-import cheerio from "https://dev.jspm.io/cheerio@1.0.0-rc.3";
+import cheerio from "npm:cheerio@1.0.0-rc.3";
 
 import { IContainHtml, setupHtmlContainer } from "./htmlAdapter.ts";
 
--- dist/ts/plainTextConverterAdapter.ts
+++ dist/ts/plainTextConverterAdapter.ts
@@ -1,6 +1,6 @@
 // This is the last typing file before the 6.0.0 major version break
 // @deno-types="https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/7046815a55ef35f365e065557b405eb2f76eff67/types/html-to-text/index.d.ts"
-import htmlToText from "https://dev.jspm.io/html-to-text@5.1.1";
+import htmlToText from "npm:html-to-text@5.1.1";
 
 import { IContainHtml } from "./htmlAdapter.ts";
 
